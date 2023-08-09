7dba536bbf6b33c61af0894728830caacfd69cb9
Jason Moore <jmoore@austwa.com>
2023-08-09 11:09:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='wagov_utils',
       author='Department of Biodiversity, Conservation and Attractions',
       author_email='asi@dbca.wa.gov.au',
       license='BSD',
-      packages=['wagov_utils','wagov_utils.components','wagov_utils.components.utils'
+      packages=['wagov_utils','wagov_utils.components','wagov_utils.components.utils','wagov_utils.components.proxy'
                 ],
       install_requires=[],
       include_package_data=True,
