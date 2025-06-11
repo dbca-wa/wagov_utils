997645fbc53c447ecaaf7b6d5f4eacd373a61a27
Juan Achury <jcachuryb@outlook.com>
2025-06-11 07:54:12 +0000

    Update of Manifest.in and module export config

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wagov_utils',
-      version='1.11',
+      version='1.11-beta',
       description='DBCA WA GOV utils',
       url='https://github.com/dbca-wa/wagov_utils',
       author='Department of Biodiversity, Conservation and Attractions',
