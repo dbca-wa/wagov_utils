4ade30edd4eb24c079c9508f50d132d51168574e
Jason Moore <jmoore@austwa.com>
2025-08-07 11:55:07 +0800

    Create security_fix_2404.sh

---
+++ wagov_utils/bin/security_fix_2404.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://mirrors.edge.kernel.org/ubuntu/pool/main/libx/libxml2/libxml2_2.12.7%2Bdfsg-3_amd64.deb -O /tmp/libxml2_2.12.7%2Bdfsg-3_amd64.deb
+dpkg -i /tmp/libxml2_2.12.7%2Bdfsg-3_amd64.deb
