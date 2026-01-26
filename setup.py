2057e2ce433905cd686b49497ac6447af921e381
Jason Moore <jason.moore@dbca.wa.gov.au>
2026-01-26 09:30:54 +0800

    Add missing folder to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='wagov_utils',
-      version='1.17',
+      version='1.18',
       description='DBCA WA GOV utils',
       url='https://github.com/dbca-wa/wagov_utils',
       author='Department of Biodiversity, Conservation and Attractions',
       author_email='asi@dbca.wa.gov.au',
       license='BSD',
-      packages=['wagov_utils','wagov_utils.components','wagov_utils.components.utils','wagov_utils.components.proxy','wagov_utils.components.middleware'
+      packages=['wagov_utils','wagov_utils.components','wagov_utils.components.utils','wagov_utils.components.proxy','wagov_utils.components.middleware','wagov_utils.components.json_auth'
                 ],
       install_requires=[],
       include_package_data=True,
