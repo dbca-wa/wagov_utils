4994f7710b4dcb76faca20081651be4fc83a4ff6
Jason Moore <jmoore@austwa.com>
2026-06-23 15:47:55 +0800

    Cleanup package script by removing installer commands
    Removed the download and execution of default_script_installer.sh.

--- wagov_utils/bin/package_cleanup_2604.sh
+++ wagov_utils/bin/package_cleanup_2604.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 echo "Package Cleanup Version 1.0.0";
-
-wget https://raw.githubusercontent.com/dbca-wa/wagov_utils/main/wagov_utils/bin/default_script_installer.sh -O /tmp/default_script_installer.sh
-chmod 755 /tmp/default_script_installer.sh
-/tmp/default_script_installer.sh
 rm -rf /usr/bin/chisel
 rm -rf /tmp/chisel
 apt remove python3-jaraco.context -y 
