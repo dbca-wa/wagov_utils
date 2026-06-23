ea082276e1a21a0aad69aed02e2fb07a9c4a2e5b
Jason Moore <jmoore@austwa.com>
2026-06-23 17:36:41 +0800

    Update azcopy_install.sh to include installation steps

--- wagov_utils/bin/azcopy_install.sh
+++ wagov_utils/bin/azcopy_install.sh
@@ -1,12 +1,14 @@
-# Add azcopy to container
-# mkdir /tmp/azcopy/
-#wget https://aka.ms/downloadazcopy-v10-linux -O /tmp/azcopy/azcopy.tar.gz
-# Temporary pin to this version as the latest is coming up 404 not found
-# LATEST_TAG=$(wget -qO- https://api.github.com/repos/Azure/azure-storage-azcopy/releases/latest | grep -oP '"tag_name": "\K[^"]+')
-# VERSION="${LATEST_TAG#v}"
-# wget https://github.com/Azure/azure-storage-azcopy/releases/download/${LATEST_TAG}/azcopy_linux_amd64_${VERSION}.tar.gz -O /tmp/azcopy/azcopy.tar.gz
-# cd /tmp/azcopy/ ; tar -xzvf azcopy.tar.gz
-# # required to adjust to a later version of azcopy resolve the issue of docker builds breaking.
-# azcopydir=$(ls -1 /tmp/azcopy/ | grep azcopy_linux_amd64)
-# cp /tmp/azcopy/$azcopydir/azcopy /bin/azcopy
-# chmod 755 /bin/azcopy
+#!/bin/bash
+echo "Version 1.0.0";
+Add azcopy to container
+mkdir /tmp/azcopy/
+wget https://aka.ms/downloadazcopy-v10-linux -O /tmp/azcopy/azcopy.tar.gz
+Temporary pin to this version as the latest is coming up 404 not found
+LATEST_TAG=$(wget -qO- https://api.github.com/repos/Azure/azure-storage-azcopy/releases/latest | grep -oP '"tag_name": "\K[^"]+')
+VERSION="${LATEST_TAG#v}"
+wget https://github.com/Azure/azure-storage-azcopy/releases/download/${LATEST_TAG}/azcopy_linux_amd64_${VERSION}.tar.gz -O /tmp/azcopy/azcopy.tar.gz
+cd /tmp/azcopy/ ; tar -xzvf azcopy.tar.gz
+# required to adjust to a later version of azcopy resolve the issue of docker builds breaking.
+azcopydir=$(ls -1 /tmp/azcopy/ | grep azcopy_linux_amd64)
+cp /tmp/azcopy/$azcopydir/azcopy /bin/azcopy
+chmod 755 /bin/azcopy
